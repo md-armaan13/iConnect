6b9954372d29cb4702398d3bf24a3c59f029074c
MD ARMAAN ANSARI <arman.kanpur4080@gmail.com>
2022-10-04 17:07:05 +0530

    showing users post int the home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,17 @@
+const Post= require('../models/post')
+
 module.exports.home= (req,res)=>{ // exporting the fuction so that router can use
-    return res.render('home',{
-        title : "Home",
+    Post.find({user :req.user._id},(err,posts)=>{
+        if(err){
+            console.log('error in fetching post from db');
+            return;
+        }
+        
+        return res.render('home',{
+            title : "Home",
+            post :posts,
+        });
     });
+    
+    
 };
--- controllers/users_posts.js
+++ controllers/users_posts.js
@@ -3,6 +3,9 @@ const Post = require('../models/post'); // including post model
 
 module.exports.Users_post=(req,res)=>{
  // creating posts
+ if(!req.isAuthenticated()){
+    return res.redirect('/user/sign-in');
+ }
     Post.create({
         content: req.body.content,
         user: req.user._id,
