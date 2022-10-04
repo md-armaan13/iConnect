c0a26417b7466ed2647d8df781431d575e3cce4b
MD ARMAAN ANSARI <arman.kanpur4080@gmail.com>
2022-10-04 17:25:49 +0530

    populating post with user, adding name of user which added post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,7 @@
 const Post= require('../models/post')
 
 module.exports.home= (req,res)=>{ // exporting the fuction so that router can use
-    Post.find({user :req.user._id},(err,posts)=>{
+   /* Post.find({},(err,posts)=>{
         if(err){
             console.log('error in fetching post from db');
             return;
@@ -11,7 +11,19 @@ module.exports.home= (req,res)=>{ // exporting the fuction so that router can us
             title : "Home",
             post :posts,
         });
+    });*/
+    // populating the user with the post
+    Post.find({}).populate('user').exec(function(err,posts){
+        if(err){
+            console.log('error in fetching post from db');
+            return;
+        }
+        
+        return res.render('home',{
+            title : "Home",
+            post :posts,
+        });
+
     });
-    
     
 };
