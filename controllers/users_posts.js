2063378e0c6ab520613f2db26389555f9260c63d
MD ARMAAN ANSARI <arman.kanpur4080@gmail.com>
2022-10-06 19:07:37 +0530

    removing post functionality,changes in user profile

--- controllers/users_posts.js
+++ controllers/users_posts.js
@@ -1,5 +1,5 @@
 const Post = require('../models/post'); // including post model
-
+const Comment =require('../models/comment');
 
 module.exports.Users_post=(req,res)=>{
  // creating posts
@@ -15,6 +15,29 @@ module.exports.Users_post=(req,res)=>{
             return;
         }
         return res.redirect('back');
+    })
+
+
+}
+
+module.exports.delete=(req,res)=>{
+    // find the post if it exist or not
+    Post.findById(req.params.id,(err,post)=>{
+        if(err){
+            console.log(err,'error in finding the post');
+            return;
+        }
+        // .id means coverting the object id into string
+        if(post.user==req.user.id){
+            post.remove();
+       
+            Comment.deleteMany({post: req.params.id},(err)=>{
+                        return res.redirect('back');
+            })
+        }else{
+            return res.redirect('back');
+        }
+
     })
 
 
--- routes/posts.js
+++ routes/posts.js
@@ -6,4 +6,6 @@ const Userspost= require('../controllers/users_posts'); // INCLUDING CONTROLLER
 
 router.post('/',Userspost.Users_post);
 
+router.get('/delete/:id',passport.checkAuthentication,Userspost.delete);
+
 module.exports= router;
