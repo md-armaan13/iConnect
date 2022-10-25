1db80c05ce2a678f41a21d299c593ab85ee4d4df
MD ARMAAN ANSARI <arman.kanpur4080@gmail.com>
2022-10-25 20:22:06 +0530

    making our first working api

--- controllers/api/v1/post_api.js
+++ controllers/api/v1/post_api.js
@@ -1,8 +1,20 @@
-module.exports.index=(req,res)=>{
+const Post= require('../../../models/post'); 
+module.exports.index = async (req,res)=>{
+
+    const posts= await Post.find({})
+    .sort({'updatedAt': -1})
+    .populate('user')
+    //population chaining 
+    .populate({
+        path:'comments', //  PATH NAME IS AS PROVIDED IN POST SCHEMA
+        populate :{
+            path: 'user'
+                }
+    });
 
 return res.json(200,{
     message : "list of posts",
-    posts :[]
+    posts : posts
 });
 
 }
