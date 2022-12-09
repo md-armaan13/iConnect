852451b36ab649a46843fa6ad6eb9d2d6bcd84cd
MD ARMAAN ANSARI <arman.kanpur4080@gmail.com>
2022-12-10 03:28:28 +0530

    some changes

--- config/kue.js
+++ config/kue.js
@@ -13,8 +13,10 @@ module.exports.emailqueue= new Queue('emails',{
 rediss://red-ce950e94rebc0ptqdpgg:WhsVNyyNnnO6MMbNdxqK55Oj0GM0o5A2@oregon-redis.render.com:6379
 module.exports.resetqueue =  new Queue('reset',{
     redis : {
-        host : '127.0.0.1',
-        port : '6379'
+        port: 6379, 
+      host: 'oregon-redis.render.com',
+      username: 'red-ce950e94rebc0ptqdpgg',
+      password : 'WhsVNyyNnnO6MMbNdxqK55Oj0GM0o5A2'
 
     }
 }) ;
